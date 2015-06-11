6b9ac6960cebea599ca63ed10ff71d2e604e8d9d
Iyanu-Tomiwa <iyanu.durotola@andela.co>
2015-06-11 23:16:13 +0100

    set up database

--- js/app.js
+++ js/app.js
@@ -1,18 +1,31 @@
 'use strict';
 
 
-var noteApp = angular.module('noteApp', ['ui.router']);
+var noteApp = angular.module('noteApp', ["firebase"]);
+noteApp.controller('homeCtrl', ['$scope','$http', function($scope,$http) {
+  var ref = new Firebase("https://andela-note.firebaseio.com");
+  var notesRef =  ref.child('notes');
+  console.log('gettin here');
+  $scope.activeNote = 1;
 
-noteApp.config('$stateProvider', function ($stateProvider) {
-  $stateProvider
-  .state('home', {
-    url: '/home',
-    templateUrl: 'view/home.html',
-    controller: 'homeCtrl'
-  })
-  .state('view', {
-    url : 'note',
-    templateUrl: 'view/view.html',
-    controller: 'viewCtrl',
-  });
-});
+  $scope.createNote = function() {
+    console.log('about to creat note');
+    var newNote = {
+      name:"adebayo",
+      content: "",
+      dateCreated: Date.now(),
+      dateUpdate: "Not Updated Yet"
+    };
+    notesRef.push(newNote);
+
+  };
+
+  $scope.toggleNote = function(index) {
+    $scope.activeNote = index;
+    console.log('gettin gere');
+
+  };
+
+  
+}]);
+
