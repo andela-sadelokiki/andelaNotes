5e4d1943cea9ea7f8f23afe3cd6d77c3267fcc63
Iyanu-Tomiwa <iyanu.durotola@andela.co>
2015-06-12 12:14:02 +0100

    setup database communication

--- js/app.js
+++ js/app.js
@@ -1,31 +1,72 @@
 'use strict';
 
 
-var noteApp = angular.module('noteApp', ["firebase"]);
-noteApp.controller('homeCtrl', ['$scope','$http', function($scope,$http) {
+var noteApp = angular.module('noteApp', ['firebase', 'ui.bootstrap']);
+noteApp.controller('homeCtrl', ['$scope','$http','$modal','$timeout','$firebaseArray', function($scope, $http, $modal, $timeout, $firebaseArray) {
   var ref = new Firebase("https://andela-note.firebaseio.com");
   var notesRef =  ref.child('notes');
   console.log('gettin here');
-  $scope.activeNote = 1;
-
-  $scope.createNote = function() {
-    console.log('about to creat note');
-    var newNote = {
-      name:"adebayo",
-      content: "",
-      dateCreated: Date.now(),
-      dateUpdate: "Not Updated Yet"
-    };
-    notesRef.push(newNote);
-
-  };
-
-  $scope.toggleNote = function(index) {
+  $scope.activeNote = 0;
+  notesRef.on('value', function(data) {
+    $timeout(function() {
+      $scope.notes = data.val();
+    }); 
+    
+  });
+  // notesRef
+  $scope.toggleNote = function(note, index) {
     $scope.activeNote = index;
+    $scope.selectedNote = note;
     console.log('gettin gere');
 
   };
 
-  
-}]);
+  $scope.updateNotes = function(notesToUpdate) {
+    notesToUpdate.dateUpdated = Date.now();
+    notesRef.child(notesToUpdate.dateCreated).update(notesToUpdate, function(error) {
+      if(error) console.log('There was an error updating the note');
+    });
+  };
+
+  $scope.modalCreate = function () {
+
+     var modalInstance = $modal.open({
+       templateUrl: 'partials/newNote.html',
+       controller: function ($scope, $modalInstance) {
+        var newNote = {};
+        $scope.ok = function () {
+          newNote.name = $scope.name;
+          newNote.description = $scope.desc;
+          newNote.dateCreated = Date.now();
+          newNote.dateUpdated = "Has not been Updated";
+          notesRef.child(newNote.dateCreated).set(newNote, function(error) {
+            if (error) {
+              alert("Data could not be saved." + error);
+            } else {
+              alert("Data saved successfully.");
+            };
+          })
+
+          $modalInstance.close();
+        };
+
+        $scope.cancel = function () {
+           $modalInstance.dismiss('cancel');
+        };
+
+        $scope.createNewNote = function(newNote) {
+          console.log('to be sure');
+          
+        };
+       },
+       size: 'bg'
+     });
+
+     modalInstance.result.then(function (selectedItem) {
+     }, function () {
+       console.log('Modal dismissed at: ' + new Date());
+     });
+   };
+
+  }]);
 
